d41c307f093c9df032d528ef0b983730473f84db
Lauren Hamel <lauren.hamel@outlook.com>
2019-04-02 13:05:40 -0400

    Adds grunt for task automation and deployment process

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = (grunt) => {
+  
+  // Load dependencies.
+  const path = require('path');
+  
+  // Configure tasks.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    secret: grunt.file.readJSON('secret.json'),
+    environments: {
+      production: {
+        options: {
+          current_symlink: 'current',
+          release_root: 'versions',
+          local_path: '_site',
+          tag: '<%= pkg.version %>',
+          deploy_path: '<%= secret.production.path %>',
+          host: '<%= secret.production.host %>',
+          username: '<%= secret.production.username %>',
+          password: '<%= secret.production.password %>',
+          port: '<%= secret.production.port %>'
+        }
+      }
+    }
+  });
+  
+  // Load tasks.
+  require('load-grunt-tasks')(grunt);
+  
+  // Register tasks.
+  grunt.registerTask('dev', 'Build and serve the Jekyll project', function () {
+
+    // Make asynchronous.
+    const done = this.async();
+    
+    // Run the `serve` Jekyll command.
+    grunt.util.spawn({
+      cmd: '~/.rbenv/shims/bundle',
+      args: ['exec', 'jekyll', 'serve'],
+      opts: {
+        stdio: 'inherit',
+        shell: true
+      }
+    }, () => done());
+    
+  });
+  grunt.registerTask('dist', 'Build the Jekyll project', function () {
+    
+    // Make asynchronous.
+    const done = this.async();
+    
+    // Run the `build` Jekyll command.
+    grunt.util.spawn({
+      cmd: '~/.rbenv/shims/bundle',
+      args: ['exec', 'jekyll', 'build'],
+      opts: {
+        stdio: 'inherit',
+        shell: true
+      }
+    }, () => done());
+    
+  });
+  grunt.registerTask('deploy', [
+    'dist', 
+    'ssh_deploy:production'
+  ]);
+  grunt.registerTask('default', ['dev']);
+  
+};
